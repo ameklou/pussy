309e45c5fb3b7896f620446c1ec1aed3aa7726b5
Eklu Kodzo <amekley@gmail.com>
2026-06-24 08:28:29 +0000

    shadcn added

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@/components/ui/button'
 
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button> Buttona </Button>
     </div>
   )
 }
